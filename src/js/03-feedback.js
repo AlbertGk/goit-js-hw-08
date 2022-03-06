b7ef783ebb8f891c2b94aa7b3fc2d4f60a9feab5
AlbertGk <albertgk01@gmail.com>
2022-03-06 16:54:26 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,6 +9,7 @@ const inputCatcher = event => {
   const {
     elements: { email, message },
   } = event.currentTarget;
+    
   localStorage.setItem(
     'feedback-form-state',
     JSON.stringify({
@@ -32,3 +33,14 @@ if (parsedInputVal.email !== "") {
 if (parsedInputVal.message !== '') {
   messageField.value = parsedInputVal.message;
 }
+
+const submitHandler = (event) => {
+    const {
+      elements: { email, message },
+    } = event.currentTarget;
+
+    console.log(`Email: ${email.value}, Message: ${message.value}`);
+    event.currentTarget.reset();
+}
+
+form.addEventListener('submit', submitHandler);
