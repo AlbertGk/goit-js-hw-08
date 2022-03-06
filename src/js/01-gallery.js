7dd7e8fea0ce4196cd629833e9192040cac45fcf
AlbertGk <albertgk01@gmail.com>
2022-03-06 10:29:00 +0100

    task 2 update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,25 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector('div.gallery');
+
+for (const el of galleryItems) {
+  const galleryItem = document.createElement('a');
+  galleryItem.classList.add('gallery__item');
+  galleryItem.href = el.original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = el.preview;
+  galleryImage.alt = el.description;
+
+  galleryBox.append(galleryItem);
+  galleryItem.append(galleryImage);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const timeUpdater = data => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+};
+
+const lastTime = localStorage.getItem('videoplayer-current-time');
+const parsedLastTime = JSON.parse(lastTime);
+
+player.on('timeupdate', _.throttle(timeUpdater, [(wait = 1000)]));
+
+player
+  .setCurrentTime(parsedLastTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  }); 
